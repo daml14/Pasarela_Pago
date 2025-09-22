d0a79a74d4b4cc2407f5f2d1160e09887c7b66ac
Diego  Andres Malpica Labrador <124366545+daml14@users.noreply.github.com>
2025-09-22 13:26:54 -0400

    Created new project

---
+++ src/main/java/com/test/pasarela_pago/PasarelaPagoApplication.java
@@ -0,0 +1,13 @@
+package com.test.pasarela_pago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasarelaPagoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasarelaPagoApplication.class, args);
+	}
+
+}
